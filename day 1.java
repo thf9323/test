90b12b4b0a95bd7dfc775a47e239c2fafa23c73f
thf9323 <80367291+thf9323@users.noreply.github.com>
2022-06-30 17:49:53 +0900

    Create day 1.java

---
+++ day 1.java
@@ -0,0 +1,121 @@
+/*
+2022.06.30 
+첫 자바 프로그래밍
+by 정한솔
+*/
+//import java.util.*;//(고정)
+class Main {
+  public static void main(String[] args) { //자바 프로그램의 시작점이다.
+    int x, y, z;
+
+    x = 63;
+    y = 18;
+    z = 52;
+
+    System.out.println("x의 값은"+ x +"입니다.");
+    System.out.println("y의 값은"+ y +"입니다.");
+    System.out.println("z의 값은"+ z +"입니다.");
+    System.out.println("합계는"+ (x + y + z) +"입니다.");
+    System.out.println("평균은"+ (x + y + z)/3 +"입니다.");
+    ///////////////////////////////////////////////////////////////////////
+    // int x;
+    // int y;
+
+    // x= 63;
+    // y= 18;
+
+    // System.out.println("x의 값은"+(x)+"입니다.");
+    // System.out.println("y의 값은"+(y)+"입니다.");
+    // System.out.println("합계는"+(x + y)+"입니다.");
+    //  System.out.println("평균은"+((x + y)/2)+"입니다.");
+
+    ////////////////////////////////////////////////////////////////////////
+    // System.out.println("첫 java 프로그램입니다.");
+    // System.out.println("화면에 출력하고 있습니다.");
+
+    // System.out.println("첫 java 프로그램입니다. 화면에 출력하고 있습니다.");
+
+    // System.out.print("첫 java 프로그램입니다.");
+    // System.out.println("화면에 출력하고 있습니다.");
+
+   
+   
+//"10을 더한 값은 " + (x + 10)+ "입니다."
+    
+   ///////////////////  퀴즈 //////////////////////////////////
+   /* System.out.println(" 금 ");
+    System.out.println(" 상 ");
+    System.out.println(" 천 ");
+    System.out.println(" 화 ");*/
+   ////////////////////////////////////////////////////////////////////////////////// 
+    
+    //System.out.println("num=%d%n",num);
+    // System.out.println("Hello world!");// hello world 출력
+    // System.out.println("코드의 실행의 중지시킬 경우도 사용");
+//////////////////////////////////////////////////////////////////////////////////////////
+    // int x = 10, y = 20; //정수형 x에 10을 할당 ,정수형 y에 20을 할당
+    // int tmp = 0; // tem변수에 0을 할당(변수를 초기화 했다.)
+
+    // System.out.println("x:"+ x + " y:" + y);//연결하여 출력 "+ ~ +""
+    // tmp = x; //tmp = 10을 할당
+    // x= y ;
+    // y = tmp ;
+    // System.out.println("x:"+ x + " y:" + y);
+
+    // int x= 10, y = 20;
+    // int tmp = 0;
+    // System.out.println("x:"+ x + " y:" + y + " tmp:" + tmp);
+    // tmp =x;//tmp 10
+    // x = y; //x = 20
+    // y = tmp; // y = 20
+    // System.out.println("x:"+ x + " y:" + y + " tmp:" + tmp);
+/////////////////////////////////////////////////////////////////////////////////////////////////
+   /* String name = "Ja" + "va"; // ja라는 문자열 ,va라는 문자열을 붙여서 문자열 변수 name에 할당한다.
+    String str = name + 8.0; // name이라는 문자열 형변수값과 8.0이라는 실수를 붙여서 문자열 변수 str에 할당.
+
+    System.out.println(name);
+    System.out.println(str);
+    System.out.println(7 + " ");
+    System.out.println(" " + 7);
+    System.out.println(7 + "");
+    System.out.println("" + 7);
+    System.out.println("" + "");
+    System.out.println(7 + 7 + "");
+    System.out.println("" + 7 + 7); //앞에 문자가 있어서 뒤에 숫자가 자동 형변환이 일어나 77로 나타난다.*/
+///////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    /*System.out.println("82 + 17 = " +82 + 17);
+    System.out.println("82 + 17 = " +(82 + 17));
+    System.out.println("82 - 17 = " +(82 - 17));  */
+////////////////////////////////////////////////////////////////////////////////////////////////////    
+ /* Scanner scanner = new Scanner (System.in); //scanner 객체생성
+
+    System.out.print("두자리 정수를 하나 입력해주세요.:");
+    String input = scanner.nextLine();
+    int num = Integer.parseInt(input);// 입력받은 문자열을 숫자로 변환 (고정)
+
+    System.out.println("입력내용 :"+input);
+    System.out.printf("num=%d%n", num);*/
+//////////////////////////////////////////////////////////////////////////////////    
+    /*Scanner stdIn = new Scanner(System.in);
+
+    System.out.print("정숫값:");
+    int x = stdIn.nextInt();
+    System.out.println(x +"를 입력했습니다.");*/
+////////////////////////////////////////////////////////////////////////////////    
+    // Scanner stdIn = new Scanner(System.in);
+
+    // System.out.print("정수값:");
+    // int x = stdIn.nextInt();
+
+    // System.out.println("10을 더한 값은 " + (x + 10)+ "입니다.");
+    // System.out.println("10을 뺀 값은 " + (x - 10)+ "입니다.");
+////////////////////////////////////////////////////////////////////////////////
+    
+    
+    
+  
+  
+  
+  }
+}
